c6e717e4c505c0bc20578b924c9208cc9f19da9d
RaviGahire <ravigahire3@gmail.com>
2025-10-16 00:20:01 +0530

    Removed unnecessary code from app component

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,14 @@
 import React from 'react'
 import { Header } from './components/Header'
-import { LocationFinder } from './components/Findlocation'
 
 function App() {
 
 
   return (
     <>
-  
+      
     <Header/>
-      <LocationFinder/>
+ 
     </>
   )
 }
